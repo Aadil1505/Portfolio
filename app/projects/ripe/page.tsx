9151087e8f15b1a80ae27af5f758f483ecafeb99
Aadil M. Alli <chainedraptor@gmail.com>
2025-11-20 17:28:16 -0500

    update

--- app/projects/ripe/page.tsx
+++ app/projects/ripe/page.tsx
@@ -1,11 +1,7 @@
-import ThatsAllFolksSVG from '@/components/global/thatsallfolks'
 import React from 'react'
 
 export default function page() {
   return (
-    <div>page
-
-      <ThatsAllFolksSVG/>
-    </div>
+    <div>page</div>
   )
 }
